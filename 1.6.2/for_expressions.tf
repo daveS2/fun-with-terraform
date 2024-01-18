fbb80f1a20e3ba6e2430fec6f4ee38d6679ed59b
daves2 <david.samuel1988@gmail.com>
2024-01-18 13:46:54 +0000

    add another for example

--- 1.6.2/for_expressions.tf
+++ 1.6.2/for_expressions.tf
@@ -22,20 +22,19 @@ variable "loops_map_of_objects" {
 locals {
   /*
 Shows a loop running against the variable loops_map_of_objects which then outputs based on a conditional.
-This shows how the loop can access objects in a map, and how the result of the conditional is returned
+This shows how the loop can access objects in a map, and how the result of the conditional is returned.
 */
-  loop_to_show_how_the_result_is_returned_one_conditional = [for item in var.loops_map_of_objects : item.name != "" ? true : false]
+  loop_to_show_how_the_result_is_returned_one_conditional = [for item in var.loops_map_of_objects : item.name == "" ? "${item.name} is true" : "${item.name} is false"]
   /*
-Shows a loop running against the variable loops_map_of_objects which then outputs based on two conditionals.
-This shows how the loop can access objects in a map, and how the result of the conditionals is returned
-The thought here is that if one passed the conditional we can look at the next item in the map object
+Shows a loop running against the variable loops_map_of_objects which then outputs based on an OR.
+This shows how the loop can access objects in a map, and how the result of the conditional is returned.
 */
-  loop_to_show_how_the_result_is_returned_two_conditionals = [for item in var.loops_map_of_objects : item.name != "" ? item.env != "" ? true : false : false]
+  loop_to_show_how_the_result_is_returned_with_an_or = [for item in var.loops_map_of_objects : item.name == "" || item.env == "" ? "${item.name},${item.env} is true" : "${item.name},${item.env} is false"]
 }
 
 output "loops_how_the_result_is_returned_from_a_for_loop_one_conditional" {
   value = local.loop_to_show_how_the_result_is_returned_one_conditional
 }
-output "loops_how_the_result_is_returned_from_a_for_loop_two_conditionals" {
-  value = local.loop_to_show_how_the_result_is_returned_two_conditionals
+output "loops_how_the_result_is_returned_from_a_for_loop_with_an_or" {
+  value = local.loop_to_show_how_the_result_is_returned_with_an_or
 }
