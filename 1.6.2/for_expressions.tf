1a48dca8f93e571c20f1f1eb77f1790058ee8b10
daves2 <david.samuel1988@gmail.com>
2024-01-18 13:35:07 +0000

    add a second conditional exmaple

--- 1.6.2/for_expressions.tf
+++ 1.6.2/for_expressions.tf
@@ -12,17 +12,30 @@ variable "loops_map_of_objects" {
       name = ""
       env  = "bar-dev"
     }
+    item3 = {
+      name = "rar"
+      env  = ""
+    }
   }
 }
 
-/*
+locals {
+  /*
 Shows a loop running against the variable loops_map_of_objects which then outputs based on a conditional.
 This shows how the loop can access objects in a map, and how the result of the conditional is returned
 */
-locals {
-  loop_to_show_how_the_result_is_returned = [for item in var.loops_map_of_objects : item.name != "" ? true : false]
+  loop_to_show_how_the_result_is_returned_one_conditional = [for item in var.loops_map_of_objects : item.name != "" ? true : false]
+  /*
+Shows a loop running against the variable loops_map_of_objects which then outputs based on two conditionals.
+This shows how the loop can access objects in a map, and how the result of the conditionals is returned
+The thought here is that if one passed the conditional we can look at the next item in the map object
+*/
+  loop_to_show_how_the_result_is_returned_two_conditionals = [for item in var.loops_map_of_objects : item.name != "" ? item.env != "" ? true : false : false]
 }
 
-output "loops_how_the_result_is_returned_from_a_for_loop"{
-  value = local.loop_to_show_how_the_result_is_returned
+output "loops_how_the_result_is_returned_from_a_for_loop_one_conditional" {
+  value = local.loop_to_show_how_the_result_is_returned_one_conditional
+}
+output "loops_how_the_result_is_returned_from_a_for_loop_two_conditionals" {
+  value = local.loop_to_show_how_the_result_is_returned_two_conditionals
 }
