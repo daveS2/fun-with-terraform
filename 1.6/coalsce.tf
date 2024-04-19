c3231a1682762e977122c74c616ba2da33436674
daves2 <david.samuel1988@gmail.com>
2024-04-19 15:21:04 +0100

    tidy up coalsce

---
+++ 1.6/coalsce.tf
@@ -0,0 +1,51 @@
+# https://developer.hashicorp.com/terraform/language/functions/coalesce
+
+locals {
+  coalsce-list-2-elements = ["picklerick", "rick"]
+  coalsce-list-empty      = []
+  coalsce-null            = null
+
+}
+
+/*
+If given a list coalsce will return the whole list if first
+*/
+output "coalsce-list" {
+  value = coalesce(local.coalsce-list-2-elements)
+}
+
+/*
+This will Fail
+They must be of same type
+output "coalsce-list-and-string" {
+    value = coalesce(local.coalsce-list-2-elements,"squanchy")
+}
+*/
+
+/*
+Straight forward string example
+*/
+output "coalsce-string-and-string" {
+  value = coalesce("birdperson", "squanchy")
+}
+
+/*
+Coalsce can work with an empty list
+*/
+output "colasce-empty-list" {
+  value = coalesce(local.coalsce-list-empty)
+}
+
+/*
+This will Fail
+output "null-coal" {
+    value = coalesce(local.coalsce-null)
+}
+*/
+
+/* 
+How to convert a null to support coalse
+*/
+output "null-coal" {
+  value = local.emptynull == null ? [] : coalesce(local.coalsce-null)
+}
